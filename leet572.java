384b16e4c275d59de4b9f1f3a0f41c10d28515a0
Suraj-Airani <surajairani@outlook.com>
2025-10-31 02:29:03 +0530

    Solution for leetcode 572: Subtree of another tree

---
+++ leet572.java
@@ -0,0 +1,28 @@
+import javax.swing.tree.TreeNode;
+
+public class leet572 {
+    class Solution {
+    private boolean helper(TreeNode root, TreeNode subRoot){
+        if(root == null && subRoot == null) return true;
+        if(root == null || subRoot == null) return false;
+        
+        if(root.val == subRoot.val){
+            return helper(root.left, subRoot.left) && helper(root.right, subRoot.right);
+        }
+
+        return false;
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(subRoot == null) return true;
+        if(root == null) return false;
+        
+        if(root.val == subRoot.val){
+            if(helper(root, subRoot)){
+                return true;
+            }
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+}
+}
